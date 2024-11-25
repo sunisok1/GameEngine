8ae332d1dd1f4b6c5606bc3b4c927823f3280c43
TKOFFICE\sunjp <sunjp@mail.jj.cn>
2024-11-25 18:00:59 +0800

    Service

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using Framework.Yggdrasil.Service;
+using Common;
+using Framework.ServiceImpl;
+using Framework.Yggdrasil;
+using Framework.Yggdrasil.Services;
 using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    private ServiceInjector m_mInjector;
+
     // Start is called before the first frame update
     void Start()
     {
-        Injector.SetInjector(new ServiceInjector());
+        m_mInjector = Injector.SetInjector<ServiceInjector>();
+        m_mInjector.Register<ILoggerService, UnityLogger>();
+        m_mInjector.Register<IEventService, EventService>();
+        m_mInjector.Register<IObjectService, ObjectService>();
+        m_mInjector.Register<IUIService, UIService>();
+    }
+
+    void Test()
+    {
+        var ui = m_mInjector.GetService<IUIService>();
+        ui.Open<FakeUI, FakeUI.FakeCreateArgs>(new());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
