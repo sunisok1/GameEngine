2e9b084be368f7e33882b9500a7b1ae309e1a382
TKOFFICE\sunjp <sunjp@mail.jj.cn>
2025-01-17 09:45:46 +0800

    简化Game Start

--- Assets/GamePacksLoader.cs
+++ Assets/GamePacksLoader.cs
@@ -1,26 +1,12 @@
-﻿using System;
-using Framework.Yggdrasil;
-using Hall;
+﻿using Hall;
 using UnityEngine;
 
 [RequireComponent(typeof(GameHall))]
-[RequireService(typeof(GamePacksLoader))]
-
-public partial class GamePacksLoader : MonoBehaviour
+public class GamePacksLoader : MonoBehaviour
 {
     private void Start()
     {
         var gameHall = GetComponent<GameHall>();
-        gameHall.CreateGameEntry(new Game.Template.GamePack());
         gameHall.CreateGameEntry(new Game.Named.GamePack());
-        var i = ;
-    }
-}
-
-public class RequireServiceAttribute : Attribute
-{
-    public RequireServiceAttribute(Type type)
-    {
-        throw new NotImplementedException();
     }
 }
